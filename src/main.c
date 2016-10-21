7cf6ddaad69a09278427df92e4fa01b3571caa45
smbct <smbct.dev@yahoo.com>
2016-10-21 23:20:55 +0200

    random generic kp

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "Solution.h"
 #include "Voisinage.h"
@@ -7,6 +8,8 @@
 
 //------------------------------------------------------------------------------
 int main(int argc, char* argv[]) {
+
+    srand((unsigned int)time(NULL));
 
     Probleme pb;
 
@@ -17,11 +20,29 @@ int main(int argc, char* argv[]) {
     Solution sol;
     creerSolution(&pb, &sol);
 
-    rechercheVND(&sol);
+    constructionGloutonne(&sol);
+
+    printf("Solution gloutonne : \n");
+    afficherSolution(&sol);
+
+    int rea = 0;
+    Solution cpy;
+    creerSolution(&pb, &cpy);
+    copierSolution(&sol, &cpy);
+    int nbIt = 0;
+    do {
+        rea = echangeAlea(&cpy, 1, 1);
+        nbIt ++;
+    } while(!rea);
+    printf("nbIt : %d\n", nbIt);
+    afficherSolution(&cpy);
+    printf("valeur z = %d\n", cpy.z);
+
+    /*rechercheVND(&sol);
 
     printf("Solution après VND : \n");
     afficherSolution(&sol);
-    printf("valeur z = %d\n", sol.z);
+    printf("valeur z = %d\n", sol.z);*/
 
     // destruction de la solution
     detruireSolution(&sol);
